e98b1cf7faedd73024852c989aa3a57c2b720aef
robinmck11 <robin.mck11@hotmail.co.uk>
2020-06-11 21:27:45 +0100

    Rewrite first fit and create entry point

--- OrderProcessing/OrderProcessor.rb
+++ OrderProcessing/OrderProcessor.rb
@@ -2,12 +2,14 @@ require 'parallel'
 require_relative 'ShippingStrategy';
 
 class OrderProcessor
-  attr_reader :fileName, :cores, :strategy
+  attr_reader :fileName, :cores, :strategy, :coreThreshold, :pretty
 
-  def initialize(fileName, cores, strategy)
+  def initialize(fileName, cores, strategy, pretty)
     @fileName = fileName
     @cores = cores
     @strategy = strategy
+    @coreThreshold = 501
+    @pretty = pretty
   end
 
   # Return array of orders
@@ -17,35 +19,30 @@ class OrderProcessor
     end
   end
 
-  def splitOrders
-    orders = getOrdersFromFile.each_slice(cores).to_a
-  end
-
-  def file_found?
-    if !fileName
-      false
-    end
-  end
-
-  def processSequential
-    arrays = splitOrders
-    arrays.each do | array |
-      array.each do |order |
-        ShippingContext.new(strategy, order).shipItems
-      end
-    end
+  def splitOrders orders
+    orders.each_slice(orders.length / cores).to_a
   end
 
   def process
+    orders = getOrdersFromFile
+
+    if orders.length < coreThreshold
+      orders.each do | order |
+        ShippingContext.new(strategy, order, pretty).shipItems
+      end
+
+      return
+    end
+
     processOrder = proc do | array |
       array.each do | order |
-        ShippingContext.new(strategy, order).shipItems
+        ShippingContext.new(strategy, order, pretty).shipItems
       end
     end
 
     processOrders = proc do
-      arrays = splitOrders
-      Parallel.map(arrays, in_processes: arrays.length) { |array|
+      orders = splitOrders(orders)
+      Parallel.map(orders, in_processes: cores) { |array|
         processOrder.call array
       }
 
